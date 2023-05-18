d90647318f66ee683b06c0dbae2527966ccb0314
denpo29 <gonza.dp29@hotmail.com>
2023-05-18 15:54:51 -0300

    Resuelta la conexión y alamcenamiento en la BD

--- index.js
+++ index.js
@@ -2,18 +2,26 @@
 const express = require('express');
 const mysql = require('mysql');
 const bodyParser = require('body-parser');
+const path = require('path');
+
 
 //crear una constante que almacene en app las funciones de express
 const app = express();
 
-//llamar a la función use para poder capturar los datos del loggin
-app.use(bodyParser.urlencoded({ extended: true }));
+app.set('view engine', 'ejs');
+
+//app.set('views', path.join(__dirname, 'views'));
+//app.use(express.static(path.join(__dirname, './registro/views')))
+app.set('views', path.join(__dirname, 'registro', 'views'));
+
+
 
 // se crea la variable connection para establecer la conexión con la BD
 const connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
   password: '',
+  port: 3306,
   database: 'topochico'
 });
 
@@ -23,22 +31,27 @@ connection.connect((err) => {
   console.log('Conectado a la base de datos!');
 });
 
+//llamar a la función use para poder capturar los datos del loggin
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 // establece la ruta a seguir para mostrar la plantilla registro, que contiene el login
-app.get('/', (req, res) => {
-  res.render('registro.ejs');
+app.get('/registro', function(req,res){
+  res.render('registro');
 });
+
 
 //Se llama al método post para pasar a la BD los datos a capturar: nombre y correo
 app.post('/registro', (req, res) => {
-  const { nombre, correo  } = req.body;
-  const usuario = { nombre, correo};
-
-  //se realiza una consulta a la BD y se da la instrucción INSERT para guardar los datos requeridos en los campos de la tabla usarios (nombre, correo)
-  connection.query('INSERT INTO usuarios SET ?', usuario, (err, result) => {
-    if (err) throw err;
-    console.log(result);
-    //res.redirect('usuario creado');
-    console.log('usuario creado')
+  const fullname = req.body.fullname;
+  const email = req.body.email;
+  connection.query('INSERT INTO usuarios SET ?', {fullname: fullname, email: email}, (error, results) => {
+    if (error) {
+      console.error('Error al cargar datos en la base de datos:', error);
+    } else {
+      console.log('Usuario guardado correctamente en la base de datos');
+      res.send('ALTA EXITOSA');
+    }
   });
 });
 
